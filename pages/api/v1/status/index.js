facd620b5beef0f05fad01ae3be993618e11b906
Vinicius Santos <viniciusjetmax@gmail.com>
2025-03-02 22:53:47 -0300

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+
   response
     .status(200)
     .json({ chave: "Alunos do curso.dev sâo acima da média" });
