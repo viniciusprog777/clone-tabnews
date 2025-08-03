69cd9433c08d6eaa32acdecaa389a91986bc83f3
Vinicius Santos <viniciusjetmax@gmail.com>
2025-08-03 13:21:28 -0300

    feat: console.log()

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("Credenciais do Postgres:", client);
 
   try {
     await client.connect();
